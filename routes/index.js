0d491aca793aa20d4d37b505768d6402c5040093
Roberto Guerrero <Roberto@MacBook-Pro-de-Roberto.local>
2017-06-04 18:54:28 +0200

    modificaciones4

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+var quizController = require('../controllers/quiz_controller');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
